785acc46cedb08e02ff3357697aedf14fcc642fd
Perfect03 <g.zlobin@ddirection.kz>
2025-05-08 13:28:43 +0500

    fix: return class (needed for some addEventListener); write inline styles

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ const YSmartCaptcha = {
       },
       render() {
         return h('div', {
-          style: 'height: 100px;',
+          style: 'height: 100px; min-width: unset',
+          class: 'smart-captcha',
           id: this.widgetId,
           'data-sitekey': options.siteKey
         });
